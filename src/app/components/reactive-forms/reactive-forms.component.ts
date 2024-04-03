c2bd5dd55c3eda121b02269cb7d999348d647cc0
ElKoloWirz96 <agustin-96@live.com.ar>
2024-04-03 19:28:23 -0300

    Desafio-03 terminado

--- src/app/components/reactive-forms/reactive-forms.component.ts
+++ src/app/components/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -13,17 +14,47 @@ export class ReactiveFormsComponent implements OnInit {
 
   ngOnInit (): void {
     this.userForm = this.fb.group ({
-      user: ['', Validators.required],
-      password: ['', Validators.required]
+      user: [
+        '',[
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'), 
+          Validators.required
+        ]
+      ],
+      password: [
+        '',[
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
+        ]
+      ]
     });
   }
 
-  onSubmit(): void {
-    alert('Iniciando sesion' + JSON.stringify(this.userForm.value));
+  get emailControl () {
+    return this.userForm.get('user');
   }
 
-  //[x: string]: any;
-  //userForm = this.formBuilder.group({
-  //User: this['formbuilder'].control(''),
-  //password: this['formbuilder'].control('')
+  get passwordControl() {
+    return this.userForm.get('password');
+  }
+
+  onSubmit(): void {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      }
+    });
+    Toast.fire({
+      icon: "success",
+      title: "Iniciando sesion"
+    });
+  }
+
+
 };
